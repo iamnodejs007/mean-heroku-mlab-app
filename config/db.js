05d66e094e3a9e6d259609f47466c10a769548f7
Zameer Ansari <xameeramir@users.noreply.github.com>
2017-03-11 06:40:05 +0530

    Added db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,6 @@
-
+    // config/db.js
+    module.exports = {
+	url: 'mongodb://testuser:testpassword@ds119250.mlab.com:19250/testingdb',
+	modulusurl: 'mongodb://sa:scotchDB@olympia.modulusmongo.net:27017/In5ybamu',
+        localUrl : 'mongodb://localhost/stencil-dev'
+    }
