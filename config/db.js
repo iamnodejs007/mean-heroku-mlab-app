4dab2a7e38c57373bf80e068d8d051fb13d01fe4
iamnodejs007 <iamnodejs007@gmail.com>
2017-07-21 12:11:08 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
     // config/db.js
     module.exports = {
-	url: 'mongodb://testuser:testpassword@ds119250.mlab.com:19250/testingdb',
+	url: 'mongodb://testuser:testpassword@ds151702.mlab.com:51702/iamnodejs007',
 	modulusurl: 'mongodb://sa:scotchDB@olympia.modulusmongo.net:27017/In5ybamu',
         localUrl : 'mongodb://localhost/stencil-dev'
     }
